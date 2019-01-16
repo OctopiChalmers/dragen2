ec07d2c3e19997348d116cf75d52ff5efc92dbfd
Agustín Mista <mista@chalmers.se>
2019-01-16 16:14:10 +0100

    Unfolded figure

--- paper/watch.sh
+++ paper/watch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #https://facebook.github.io/watchman/docs/watchman-make.html
-watchman-make --make 'make' -p '**/[^.]*.tex' '**/[^.]*.bib' '**/GNUmakefile' -t all
+watchman-make --make 'make' -p 'main.lhs.tex' 'notation.fmt' 'sections/[^.]*.tex' 'tikz/[^.]*.tex' '**/[^.]*.bib' '**/GNUmakefile' -t all
