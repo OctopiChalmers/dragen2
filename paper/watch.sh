08e09dd571e162e894db720e946b725e3ea82bf2
Alejandro Russo <russo@chalmers.se>
2019-01-22 09:50:59 +0100

    Sections no more

--- paper/watch.sh
+++ paper/watch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #https://facebook.github.io/watchman/docs/watchman-make.html
-watchman-make --make 'make' -p 'main.lhs.tex' 'notation.fmt' 'sections/[^.]*.tex' 'tikz/[^.]*.tex' '**/[^.]*.bib' '**/GNUmakefile' -t all
+watchman-make --make 'make' -p 'main.lhs.tex' 'notation.fmt' '*.tex' 'tikz/[^.]*.tex' '**/[^.]*.bib' '**/GNUmakefile' -t all
