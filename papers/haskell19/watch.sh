d969b281a82125509c0269c97708f98da4c11647
Agustín Mista <mista@chalmers.se>
2019-03-20 22:44:07 +0100

    Added template with base structure and a bunch of code

---
+++ papers/haskell19/watch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#https://facebook.github.io/watchman/docs/watchman-make.html
+watchman-make --make 'make' -p 'main.lhs.tex' 'notation.fmt' '[^.]*.tex' 'tikz/*.tex' '**/[^.]*.bib' '**/GNUmakefile' -t all
