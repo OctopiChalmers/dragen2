011ebcd0a22fca66eb4b1eb51db671d3ae499a33
Agustín Mista <mista@chalmers.se>
2019-06-24 16:57:13 +0200

    Removed papers from this repo

--- papers/haskell19/watch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#https://facebook.github.io/watchman/docs/watchman-make.html
-watchman-make --make 'make' -p 'main.lhs.tex' 'notation.fmt' '[^.]*.tex' 'tikz/*.tex' '**/[^.]*.bib' '**/GNUmakefile' -t all
